39ccf10de77b190a7b1859f9a36327c3a8dd8147
javad5837 <82036195+javad5837@users.noreply.github.com>
2021-04-07 00:03:15 +0430

    Update skip.ts

--- src/handlers/skip.ts
+++ src/handlers/skip.ts
@@ -9,5 +9,5 @@ export const skipHandler = Composer.command('skip', ctx => {
     }
 
     const skipped = skip(chat.id);
-    ctx.reply(skipped ? 'Skipped.' : "There's no song playing.");
+    ctx.reply(skipped ? 'تموم شد' : "There's no song playing.");
 });
