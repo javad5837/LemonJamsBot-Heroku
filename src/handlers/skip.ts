f6005b0c7dea49241e4d9de72443f1d256c83e4b
javad5837 <82036195+javad5837@users.noreply.github.com>
2021-04-07 00:11:20 +0430

    Update skip.ts

--- src/handlers/skip.ts
+++ src/handlers/skip.ts
@@ -1,13 +1,1 @@
-import { Composer } from 'telegraf';
-import { skip } from '../tgcalls';
 
-export const skipHandler = Composer.command('skip', ctx => {
-    const { chat } = ctx.message;
-
-    if (chat.type !== 'supergroup') {
-        return;
-    }
-
-    const skpped = skip(chat.id);
-    ctx.reply(ss ? 'تموم شد' : "There's no song playing.");
-});
